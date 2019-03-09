24742024d2317f601f29ec2719f7d43f0e420744
sumeirat <44616178+sumeirat@users.noreply.github.com>
2019-03-09 10:10:43 -0500

    updated

--- JavaScript_DOManipulation/static/js/app.js
+++ JavaScript_DOManipulation/static/js/app.js
@@ -10,13 +10,40 @@ submit.on("click", function() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime-input");
-
     // Get the value property of the input element
-    var dateInputValue = inputElement.property("value");
+    var dateInputValue = d3.select("#datetime-input").property("value");
+    var cityInputValue = d3.select("#city-input").property("value");
+    var stateInputValue = d3.select("#state-input").property("value");
+    var countryInputValue = d3.select("#country-input").property("value");
+    var shapeInputValue = d3.select("#shape-input").property("value");
 
-    var filteredData = tableData.filter((item) => item.datetime == dateInputValue);
+    console.log("City Input Value:" + cityInputValue + ".");
+    if(cityInputValue != ""){
+        console.log("City Input Value Inside NULL:" + cityInputValue + ".");
+    }
+    console.log("Type of: " + typeof cityInputValue )
+
+    // Filter Fuction for Table
+    var filteredData = tableData.filter((item) => {
+        var return_value = true;
+        if (dateInputValue != ""){
+            return_value = return_value && (item.datetime == dateInputValue);
+        }
+        if (cityInputValue != "" != ""){
+            return_value = return_value && (item.city == cityInputValue);
+        }
+        if (stateInputValue != ""){
+            return_value = return_value && (item.state == stateInputValue);
+        }
+        if (countryInputValue != ""){
+            return_value = return_value && (item.country == countryInputValue);
+        }
+        if (shapeInputValue != ""){
+            return_value = return_value && (item.shape == shapeInputValue);
+        }
+
+        return return_value;
+    });
 
     // Selecting table body tag and clearing all previous results
     var tbody = d3.select("#ufo-table-body");
